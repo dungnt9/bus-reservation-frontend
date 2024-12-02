14b1def93d429cdadad8b78e03788d5d9cfd90f1
Dũng Nguyễn <dungntelcom@gmail.com>
2024-12-02 13:42:04 +0700

    feat: create apiServices.js and .env

---
+++ src/services/apiServices.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const APP_URL = import.meta.env.VITE_APP_API_URL;
+
+const api = axios.create({
+  baseURL: APP_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  }
+})
+
+api.interceptors.response.use(
+  response => response.data.data,
+  error => {
+    console.error('API Error:', error)
+    return Promise.reject(error)
+  }
+)
+
+export default {
+
+}
