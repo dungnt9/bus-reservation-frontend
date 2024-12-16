9679de8a37fb75e7f3a6636d3bac75d3bc55a406
Dũng Nguyễn <dungntelcom@gmail.com>
2024-12-16 22:20:38 +0700

    update các route

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import BookTicket from '@/views/BookTicket.vue'
 import TrackTrip from '@/views/TrackTrip.vue'
 import Account from '@/views/Account.vue'
 import Invoice from '@/views/Invoice.vue'
-import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Home', component: HomeView },
-    { path: '/book', name: 'BookTicket', component: BookTicket },
     { path: '/track', name: 'TrackTrip', component: TrackTrip },
     { path: '/account', name: 'Account', component: Account },
     { path: '/invoice', name: 'Invoice', component: Invoice },
-    { path: '/about', name: 'About', component: AboutView },
   ],
 })
 
